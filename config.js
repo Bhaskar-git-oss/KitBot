10b918ad9021cd2a1c5d7c3c98c2d6bc4112a33b
Chunk <177588402+Bhaskar-git-oss@users.noreply.github.com>
2026-03-03 22:27:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+{
+  "host": "alt2.6b6t.org",
+
+  "port": 25565,
+
+  "username": "(user)",
+
+  "auth": "offline",
+
+  "loginCommand": "/login (pass)",
+
+  "allowedPlayers": ["player1", "player2", "player3"],
+
+  "kitChests": {
+    "kit5": { "x": a1, "y": b1, "z": c1 },
+    "kit4": { "x": a2, "y": b2, "z": c2 },
+    "kit3": { "x": a3, "y": b3, "z": c3 },
+    "kit2": { "x": a4, "y": b4, "z": c4 },
+    "kit1": { "x": a5, "y": b5, "z": c5 }
+  },
+
+  "maxKits": 6 //default 6
+}
