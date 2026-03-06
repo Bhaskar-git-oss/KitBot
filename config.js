0bc9adb5a87cb8c2d925547a19fc62080cf9218f
Chunk <177588402+Bhaskar-git-oss@users.noreply.github.com>
2026-03-06 19:09:07 +0530

    possible fix

--- config.js
+++ config.js
@@ -1,16 +1,11 @@
 {
-  "host": "alt2.6b6t.org",
-
+  "host": "play.6b6t.org",
   "port": 25565,
-
   "username": "(user)",
-
   "auth": "offline",
-
   "loginCommand": "/login (pass)",
 
   "allowedPlayers": ["player1", "player2", "player3"],
-
   "kitChests": {
     "kit5": { "x": a1, "y": b1, "z": c1 },
     "kit4": { "x": a2, "y": b2, "z": c2 },
@@ -18,6 +13,5 @@
     "kit2": { "x": a4, "y": b4, "z": c4 },
     "kit1": { "x": a5, "y": b5, "z": c5 }
   },
-
   "maxKits": 6 //default 6
 }
