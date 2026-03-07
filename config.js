a38636692b74a0d8fda6891d00ccbb1047ce11e3
Chunk <177588402+Bhaskar-git-oss@users.noreply.github.com>
2026-03-07 10:38:52 +0530

    Modify config.js for bot settings and features
    Updated configuration for username, login command, allowed players, max kits, and added reconnect and modules settings.

--- config.js
+++ config.js
@@ -1,17 +1,35 @@
 {
   "host": "play.6b6t.org",
   "port": 25565,
-  "username": "(user)",
+  "username": "YourBotName",
   "auth": "offline",
-  "loginCommand": "/login (pass)",
-
-  "allowedPlayers": ["player1", "player2", "player3"],
+  "loginCommand": "/login YourPassword",
+  "allowedPlayers": ["player1", "player2"],
   "kitChests": {
-    "kit5": { "x": a1, "y": b1, "z": c1 },
-    "kit4": { "x": a2, "y": b2, "z": c2 },
-    "kit3": { "x": a3, "y": b3, "z": c3 },
-    "kit2": { "x": a4, "y": b4, "z": c4 },
-    "kit1": { "x": a5, "y": b5, "z": c5 }
+    "tools": { "x": 0, "y": 0, "z": 0 },
+    "armor": { "x": 0, "y": 0, "z": 0 },
+    "pvp3": { "x": 0, "y": 0, "z": 0 },
+    "pvp2": { "x": 0, "y": 0, "z": 0 },
+    "pvp1": { "x": 0, "y": 0, "z": 0 }
   },
-  "maxKits": 6 //default 6
+  "maxKits": 9,
+  "reconnect": {
+    "enabled": true,
+    "delay": 5000,
+    "maxAttempts": 5
+  },
+  "portalWalkDistance": 14,
+  "modules": {
+    "kitBot": true,
+    "autoMessages": true,
+    "headMovement": true
+  },
+  "autoMessages": {
+    "interval": 60000,
+    "messages": [
+      "New to 6b6t? Join 'The Helpful Clan'. The chillest clan on 6b6t. https://discord.gg/EW2DuCj82X",
+      "Are you interested in Stash/Base Hunting? Join THC! https://discord.gg/EW2DuCj82X",
+      "Interested in our Bot? Join us at https://discord.gg/EW2DuCj82X for the best open-source kitbot!"
+    ]
+  }
 }
